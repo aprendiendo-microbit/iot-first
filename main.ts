68ce7e4a3e03ff8b20619281f282f41405fe59d8
Enrique Crespo <jecrespom@gmail.com>
2024-03-09 09:08:10 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+basic.showNumber(9)
+ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
+ESP8266_IoT.connectWifi("AndroidAP4628", "esp8266wifi")
+basic.showNumber(8)
+basic.forever(function () {
+    if (ESP8266_IoT.wifiState(true)) {
+        basic.showNumber(1)
+    } else {
+        basic.showNumber(0)
+    }
+    basic.pause(1000)
+    basic.showString("Hello!")
+    basic.pause(1000)
+})
